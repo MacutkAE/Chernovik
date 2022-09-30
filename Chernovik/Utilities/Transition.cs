901f9dd2509b0164cfa01e7b2b53d31578b203a0
315 <315@IT_DE-4>
2022-09-30 13:53:27 +0300

    Добавьте файлы проекта.

---
+++ Chernovik/Utilities/Transition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chernovik.Entities;
+using System.Windows.Controls;
+
+namespace Chernovik.Utilities
+{
+    internal class Transition
+    {
+        public static Frame mainFrame { get; set; }
+        private static DraftEntities1 _context {  get; set; }
+        public static DraftEntities1 Context
+        {
+            get
+            {
+                if (_context == null)
+                {
+                    _context = new DraftEntities1();
+                }
+                return _context;
+            }
+        }
+    }
+}
